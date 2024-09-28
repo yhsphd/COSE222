74851d2d34183fcf2d158f58f5131dbb93954c49
Yoon Hyungseok <yhsphd05@gmail.com>
2024-09-28 10:31:39 +0900

    20240928 Lab1

---
+++ Lab1/sub2/sub2.sv
@@ -0,0 +1,6 @@
+module sub2 (a, b, c);
+    input [3:0] a, b;
+    output [3:0] c;
+    
+    assign c = a - b;
+endmodule 
---
+++ Lab1/sub2/tb_sub2.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <iostream>
+#include <verilated.h>
+#include <verilated_vcd_c.h> // required for generating waveforms
+#include "Vsub2.h" // “sub2” is the module name
+
+#define ITER 16
+
+int main(int argc, char** argv, char** env) {
+    Vsub2 *dut = new Vsub2;
+    
+    // initializing waveform dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *m_trace = new VerilatedVcdC;
+    dut->trace(m_trace, 5);
+    m_trace->open("wave.vcd");
+    
+    // test vectors
+    for (int i = 0; i < ITER; i++) {
+        dut->a = i;
+        dut->b = ITER-1-i;
+        dut->eval();
+        m_trace->dump(i);
+        printf("a: 0x%X, b: 0x%X, c: 0x%X\n", dut->a, dut->b, dut->c);
+    }
+    dut->eval();
+    m_trace->dump(ITER);
+    
+    // closing
+    m_trace->close();
+    delete dut;
+    exit(EXIT_SUCCESS);
+}
